65fa77d1a49701a6b78fecf1e77b244ba1ab351e
Ahmed Mohamed <131635923+Ahmedmohamed998@users.noreply.github.com>
2025-01-20 05:07:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if "logged_in" in st.session_state and st.session_state.logged_in:
 else:
     st.set_page_config(page_title="Login")
 def login_page():
-    col1, col2, col3 = st.columns([1, 3, 1])  
+    col1, col2, col3 = st.columns([1, 1, 1])  
     with col2:
         st.image("login.png", width=200)
     st.markdown("<h1 style='text-align: center; color: white; margin-top: 30px; '>Login Page</h1>", unsafe_allow_html=True)
