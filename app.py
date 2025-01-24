823dfd23316f1430c0c05a3c8fe2b3b118e5a7cc
Ahmed Mohamed <131635923+Ahmedmohamed998@users.noreply.github.com>
2025-01-24 04:51:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -227,24 +227,7 @@ if page == "Completed Orders":
             results= cursor.fetchall()
             if results:
                 st.write("Search Results:")
-                data=[]
-                for order in results:
-                    data.append({
-                       "Order Number": order[0],
-                       "Customer Name": order[1],
-                       "Phone 1": order[2],
-                       "Phone 2": order[3],
-                       "Email": order[4],
-                       "Shipping Company": order[5],
-                       "Region": order[6],
-                       "Order Price": f"{order[7]}",
-                       "Shipping Price":order[10],
-                       "Order Profit": (order[7] or 0) - (order[10] or 0),
-                       "Days to Receive":order[8],
-                       "Number of Products":order[9]
-                     })
-                df = pd.DataFrame(data)
-                st.dataframe(df)
+                st.table(results)
                 total_price = sum(order[7] for order in results)
                 total_hoodies = sum(int(order[9]) for order in results)
                 st.write(f"Total Amount Spent: {total_price}")
