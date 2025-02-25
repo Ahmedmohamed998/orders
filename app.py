9929bb6c720695daf0bcd576958d35b9db2699bd
Ahmed Mohamed <131635923+Ahmedmohamed998@users.noreply.github.com>
2025-02-25 20:40:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ def orders_management_page():
         total_shipping_returned_query = """
             SELECT 
                 COUNT(o.order_number) AS total_orders, 
-                COALESCE(SUM(o.shipping_price), 0)-COALESCE(SUM(o.customer_shipping_price), 0) AS total_shipping_price
+                COALESCE(SUM(o.shipping_price), 0)-COALESCE(SUM(o.customer_shipping_price), 0) AS total_shipping_price,
                 COALESCE(SUM(o.shipping_price), 0)AS total_shipping_pricee
             FROM returned_orders o;
           """
