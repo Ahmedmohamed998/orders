7c8040afbcb014d85c59aaeaa53210a394d7c7b8
Ahmed Mohamed <131635923+Ahmedmohamed998@users.noreply.github.com>
2025-07-23 23:05:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def create_connection():
         user=db_user,
         password=db_password,
         dbname=db_name,
-        port=35051 
+        port=23208 
     )
 
 if "logged_in" in st.session_state and st.session_state.logged_in:
